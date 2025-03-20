f4cea7a3e87f62182603a8258b097dc929a3022b
hamzamak19 <hamzamak19@gmail.com>
2025-03-20 23:45:35 +0100

    Allure reporting changes

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,5 +1,6 @@
 package runner;
 
+import io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
@@ -8,7 +9,11 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        plugin = {"pretty", "html:target/cucumber-reports.html"}
+        plugin = {"pretty",
+                "json:target/allure-results/cucumber.json",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        },
+        monochrome = true
 )
 public class TestRunner {
 }
